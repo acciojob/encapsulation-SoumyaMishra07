2cc782f33f679a16754cb7df60da75906dcdca18
SoumyaMishra07 <soumyarmishra065@gmail.com>
2024-02-12 13:07:08 +0530

    Added

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
-
 public class RWOnly {
-    
+    private String name;
+
+    // Getter method
+    public String getName() {
+        return name;
+    }
+
+    // Setter method
+    public void setName(String name) {
+        this.name = name;
+    }
 }
